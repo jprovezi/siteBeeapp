5cc3a27184f573f9e8a78ed7f649ae0fd3a8416e
João Provezi <jprovezi@gmail.com>
2024-04-05 09:08:18 -0300

    arrumado css do menu mobile

--- resources/views/site/navbar.blade.php
+++ resources/views/site/navbar.blade.php
@@ -12,20 +12,19 @@
             <div class="offcanvas-body">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a href="#conheca" class="nav-link">Conheça o Beeapp</a>
+                        <a href="#conheca" class="nav-link nav-link-rodape">Conheça o Beeapp</a>
                     </li>
                     <li class="nav-item">
-                        <a href="#planos" class="nav-link">Planos</a>
+                        <a href="#planos" class="nav-link nav-link-rodape">Planos</a>
                     </li>
                     <li class="nav-item">
-                        <a href="#depoimentos" class="nav-link">Depoimentos</a>
+                        <a href="#depoimentos" class="nav-link nav-link-rodape">Depoimentos</a>
                     </li>
                 </ul>
             </div>
             <div class="offcanvas-header border-top">
-                <a href="#fale-conosco"
-                    class="btn btn-primary w-100" target="_blank" rel="noopener">
-                    <i class="bx bx-cart fs-4 lh-1 me-1"></i>
+                <a href="{{config('APP_URL')}}#contato"
+                    class="btn btn-primary w-100" rel="noopener">
                     Fale Conosco
                 </a>
             </div>
